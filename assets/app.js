e6c5c123c3df05a8f43cf6fbeb79020c41194dae
idrissa-sall <idsall.pro@gmail.com>
2024-05-25 22:59:46 +0200

    feat: v1 de la page index

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,31 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+$(function(){
+    // owl-carousel js
+    $(".owl-carousel").owlCarousel({
+        //Basic Speeds
+        slideSpeed : 200,
+        paginationSpeed : 800,
+     
+        //Autoplay
+        autoPlay : true,
+        goToFirst : true,
+        goToFirstSpeed : 1000,
+     
+        // Navigation
+        navigation : false,
+        navigationText : ["prev","next"],
+        pagination : false,
+        paginationNumbers: false,
+     
+        // Responsive
+        responsive: true,
+        items : 4,
+        itemsDesktop : [1199,4],
+        itemsDesktopSmall : [980,3],
+        itemsTablet: [768,2],
+        itemsMobile : [479,1]
+    });
+    // end
+})
